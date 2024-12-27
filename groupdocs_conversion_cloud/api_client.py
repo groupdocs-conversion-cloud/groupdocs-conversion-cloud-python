1728568f3c7a84a1fa6ec4ded9a644002daeebf5
product-team <product.team@groupdocs.com>
2024-12-27 02:24:07 -0600

    Updated sources

--- groupdocs_conversion_cloud/api_client.py
+++ groupdocs_conversion_cloud/api_client.py
@@ -74,12 +74,12 @@ class ApiClient(object):
         self.configuration = configuration
         self.pool = None
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-groupdocs-client': 'python sdk', 'x-groupdocs-version': '24.11'}
+        self.default_headers = {'x-groupdocs-client': 'python sdk', 'x-groupdocs-version': '24.12'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 24.11'
+        self.user_agent = 'python sdk 24.12'
 
     def __del__(self):
         if self.pool is not None:
