ec9351c04261d47691dfa695cc4198583158400a
product-team <product.team@groupdocs.com>
2025-10-17 03:02:55 -0500

    Updated sources

--- groupdocs_conversion_cloud/models/gis_load_options.py
+++ groupdocs_conversion_cloud/models/gis_load_options.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # -----------------------------------------------------------------------------------
-# <copyright company="Aspose Pty Ltd" file="OneLoadOptions.py">
+# <copyright company="Aspose Pty Ltd" file="GisLoadOptions.py">
 #   Copyright (c) Aspose Pty Ltd
 # </copyright>
 # <summary>
@@ -32,9 +32,9 @@ import six
 
 from groupdocs_conversion_cloud.models import LoadOptions
 
-class OneLoadOptions(LoadOptions):
+class GisLoadOptions(LoadOptions):
     """
-    One document load options
+    Options for loading Gis documents
     """
 
     """
@@ -45,108 +45,83 @@ class OneLoadOptions(LoadOptions):
                             and the value is json key in definition.
     """
     swagger_types = {
-        'default_font': 'str',
-        'font_substitutes': 'dict(str, str)',
-        'password': 'str'
+        'width': 'int',
+        'height': 'int'
     }
 
     attribute_map = {
-        'default_font': 'DefaultFont',
-        'font_substitutes': 'FontSubstitutes',
-        'password': 'Password'
+        'width': 'Width',
+        'height': 'Height'
     }
 
-    def __init__(self, default_font=None, font_substitutes=None, password=None, **kwargs):  # noqa: E501
-        """Initializes new instance of OneLoadOptions"""  # noqa: E501
+    def __init__(self, width=None, height=None, **kwargs):  # noqa: E501
+        """Initializes new instance of GisLoadOptions"""  # noqa: E501
 
-        self._default_font = None
-        self._font_substitutes = None
-        self._password = None
+        self._width = None
+        self._height = None
 
-        if default_font is not None:
-            self.default_font = default_font
-        if font_substitutes is not None:
-            self.font_substitutes = font_substitutes
-        if password is not None:
-            self.password = password
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
 
-        base = super(OneLoadOptions, self)
+        base = super(GisLoadOptions, self)
         base.__init__(**kwargs)
 
         self.swagger_types.update(base.swagger_types)
         self.attribute_map.update(base.attribute_map)
     
     @property
-    def default_font(self):
+    def width(self):
         """
-        Gets the default_font.  # noqa: E501
+        Gets the width.  # noqa: E501
 
-        Default font for Note document. The following font will be used if a font is missing.  # noqa: E501
+        Page width for converting GIS document. Default is 1000.  # noqa: E501
 
-        :return: The default_font.  # noqa: E501
-        :rtype: str
+        :return: The width.  # noqa: E501
+        :rtype: int
         """
-        return self._default_font
+        return self._width
 
-    @default_font.setter
-    def default_font(self, default_font):
+    @width.setter
+    def width(self, width):
         """
-        Sets the default_font.
+        Sets the width.
 
-        Default font for Note document. The following font will be used if a font is missing.  # noqa: E501
+        Page width for converting GIS document. Default is 1000.  # noqa: E501
 
-        :param default_font: The default_font.  # noqa: E501
-        :type: str
+        :param width: The width.  # noqa: E501
+        :type: int
         """
-        self._default_font = default_font
+        if width is None:
+            raise ValueError("Invalid value for `width`, must not be `None`")  # noqa: E501
+        self._width = width
     
     @property
-    def font_substitutes(self):
+    def height(self):
         """
-        Gets the font_substitutes.  # noqa: E501
+        Gets the height.  # noqa: E501
 
-        Substitute specific fonts when converting Note document.  # noqa: E501
+        Page height for converting GIS document. Default is 1000.  # noqa: E501
 
-        :return: The font_substitutes.  # noqa: E501
-        :rtype: dict(str, str)
+        :return: The height.  # noqa: E501
+        :rtype: int
         """
-        return self._font_substitutes
+        return self._height
 
-    @font_substitutes.setter
-    def font_substitutes(self, font_substitutes):
+    @height.setter
+    def height(self, height):
         """
-        Sets the font_substitutes.
+        Sets the height.
 
-        Substitute specific fonts when converting Note document.  # noqa: E501
+        Page height for converting GIS document. Default is 1000.  # noqa: E501
 
-        :param font_substitutes: The font_substitutes.  # noqa: E501
-        :type: dict(str, str)
+        :param height: The height.  # noqa: E501
+        :type: int
         """
-        self._font_substitutes = font_substitutes
-    
-    @property
-    def password(self):
-        """
-        Gets the password.  # noqa: E501
-
-        Set password to unprotect protected document  # noqa: E501
-
-        :return: The password.  # noqa: E501
-        :rtype: str
-        """
-        return self._password
-
-    @password.setter
-    def password(self, password):
-        """
-        Sets the password.
-
-        Set password to unprotect protected document  # noqa: E501
-
-        :param password: The password.  # noqa: E501
-        :type: str
-        """
-        self._password = password
+        if height is None:
+            raise ValueError("Invalid value for `height`, must not be `None`")  # noqa: E501
+        self._height = height
 
     def to_dict(self):
         """Returns the model properties as a dict"""
@@ -182,7 +157,7 @@ class OneLoadOptions(LoadOptions):
 
     def __eq__(self, other):
         """Returns true if both objects are equal"""
-        if not isinstance(other, OneLoadOptions):
+        if not isinstance(other, GisLoadOptions):
             return False
 
         return self.__dict__ == other.__dict__
